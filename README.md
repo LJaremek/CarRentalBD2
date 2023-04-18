5f7fe405b7ebe5ed3b517c4b87499dc24dccfd22
Lukasz Jaremek <Jaremek.Lukasz@gmail.com>
2023-04-18 19:39:42 +0200

    add first models

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # CarRentalBD2
 Car Rental for Bazy Danych 2
+
+
+## Instalacja wymaganych bibliotek
+```
+pip install -r requirements.txt
+```
+
+## Robienie migracji
+```
+python .\manage.py makemigrations carrental
+python .\manage.py migrate
+```
+
+## Uruchomienie servera
+```
+python .\manage.py runserver
+```
+
+## Tworzenie admina
+```
+python .\manage.py createsuperuser
+```
--- carrentalbd2/carrental/models.py
+++ carrentalbd2/carrental/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+CAR_STATUS = (
+    ("free", "Free"),
+    ("rented", "Rented"),
+    ("service", "Service"),
+    ("pending", "Pending"),
+)
+
+
+class CarInfo(models.Model):
+    car_brand = models.CharField(max_length=50)
+    car_type = models.CharField(max_length=50)
+    seats_number = models.IntegerField()
+    doors_number = models.IntegerField()
+    capacity = models.IntegerField()  # litres
+
+
+class CarModel(models.Model):
+    name = models.CharField(max_length=50)
+    car_info = models.ForeignKey(CarInfo, null=True, on_delete=models.SET_NULL)
+
+
+class Car(models.Model):
+    car_status = models.CharField(
+        choices=CAR_STATUS,
+        default="free",
+        max_length=15
+    )
+
+    car_model = models.ForeignKey(
+        CarModel,
+        null=True,
+        on_delete=models.SET_NULL
+        )
+
+    produced_date = models.DateTimeField()
+
+
+class User(models.Model):
+    pass
+
+
+class Rental(models.Model):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(blank=True, null=True)
+    car = models.OneToOneField(Car, null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
