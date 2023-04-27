751c29e656eee5ece995c603e5ad5d8b39dc5e55
Piotras laptop <Piotr.kow.59@gmail.com>
2023-04-27 17:04:24 +0200

    Report, Rental, User models created

--- carrentalbd2/carrental/models.py
+++ carrentalbd2/carrental/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from phone_field import PhoneField
+from django.core.validators import MaxValueValidator
 
 CAR_STATUS = (
     ("free", "Free"),
@@ -6,6 +8,27 @@ CAR_STATUS = (
     ("service", "Service"),
     ("pending", "Pending"),
 )
+
+RENTAL_STATUS = (
+    ("finished", "Finished"),
+    ("ongoing", "Ongoing"),
+    ("booked", "Booked"),
+)
+
+class User(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    login = models.CharField(max_length=50, unique=True)
+    #może zamiast tego emial?
+    #email = models.EmailField(max_length=50, unique=True)
+    password = models.CharField(max_length=50, null=False) #to do form, hash
+    phone = PhoneField(blank=True, unique=True)
+    citizenship = models.CharField(max_length=50, null=False)
+    document_name = models.CharField(max_length=50)
+    document_id = models.CharField(max_length=50)
+
+
+class Rental_Base(models.Model):
+    location = models.CharField(max_length=100, unique=True, null=False)
 
 
 class CarInfo(models.Model):
@@ -36,13 +59,27 @@ class Car(models.Model):
 
     produced_date = models.DateTimeField()
 
-
-class User(models.Model):
-    pass
-
-
 class Rental(models.Model):
     start_date = models.DateTimeField()
     end_date = models.DateTimeField(blank=True, null=True)
     car = models.OneToOneField(Car, null=True, on_delete=models.SET_NULL)
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    rental_base = models.ForeignKey(Rental_Base, null=False, on_delete=models.SET_NULL)
+
+
+class Report(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.SET_NULL, null=False)
+    rental = models.OneToOneField(Rental, on_delete=models.SET_NULL, null=False)
+    description = models.CharField(max_length=255, null=False)
+    rate = models.PositiveIntegerField(
+        validators=[MaxValueValidator(10)],
+        null=False)
+
+
+
+
+
+
+
+
+
