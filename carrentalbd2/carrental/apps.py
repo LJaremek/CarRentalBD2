13b08b4f3fc391b803ce99b6dd0d19be28595c19
lwojcic1 <01169261@pw.edu.pl>
2023-04-28 13:34:53 +0200

    structure change so makemigrations wont throw an error

--- carrentalbd2/carrental/apps.py
+++ carrentalbd2/carrental/apps.py
@@ -1,30 +1,9 @@
 from django.apps import AppConfig
-from django.core.management import call_command
+
 
 class CarrentalConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'carrental'
 
     def ready(self):
-
-        model_files = [
-            'CarType.json',
-            'Brand.json',
-            'PriceList.json',
-            'CarModel.json',
-            'Client.json',
-            'Person.json',
-            'Company.json',
-            'RentalStation.json',
-            'Car.json',
-            'Insurance.json',
-            'InsuranceCompany.json',
-            'RepairWorkshop.json',
-            'Repair.json',
-            'Rental.json',
-            'TrafficViolation.json',
-            'Raport.json',
-            'Refund.json'
-        ]
-        for file in model_files:
-            call_command('loaddata', file)
+        import carrental.signals
