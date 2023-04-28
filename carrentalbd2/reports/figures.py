2dbe95f26b8cb9091262875995de03e602291785
lwojcic1 <01169261@pw.edu.pl>
2023-04-28 21:23:07 +0200

    added last plot

--- carrentalbd2/reports/figures.py
+++ carrentalbd2/reports/figures.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from carrental.models import Brand, Repair
+from carrental.models import Brand, Repair, Car, CarModel
 from django.db.models import Count
 
 
@@ -33,4 +33,20 @@ def repair_realization():
     ax.bar(realizator, numbers)
     ax.set_ylabel("amount")
     ax.set_title("Number of realized repairs by realizator")
+    return fig
+
+
+def car_by_brand():
+    fig, ax = plt.subplots()
+    brand_car_counts = Car.objects.values("car_model__brand_id__name").annotate(
+        total_cars=Count("car_model__brand_id")
+    )
+    brand_names = []
+    car_counts = []
+    for brand_count in brand_car_counts:
+        brand_names.append(brand_count["car_model__brand_id__name"])
+        car_counts.append(brand_count["total_cars"])
+    ax.bar(brand_names, car_counts)
+    ax.set_ylabel("amount")
+    ax.set_title("Amount of possessed cars of each brand")
     return fig
--- carrentalbd2/reports/models.py
+++ carrentalbd2/reports/models.py
@@ -2,9 +2,13 @@ from django.db import models
 from django_matplotlib import MatplotlibFigureField
 
 
-class CarBrand(models.Model):
+class BrandOrigin(models.Model):
     figure = MatplotlibFigureField(figure="car_brands")
 
 
 class RepairRealization(models.Model):
     figure = MatplotlibFigureField(figure="repair_realization")
+
+
+class CarByBrand(models.Model):
+    figure = MatplotlibFigureField(figure="car_by_brand")
