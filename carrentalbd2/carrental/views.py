4dfb67f8de0a3bac2077929a107e8d26e144eae8
lwojcic1 <01169261@pw.edu.pl>
2023-05-25 16:27:22 +0200

    Form for company added

--- carrentalbd2/carrental/views.py
+++ carrentalbd2/carrental/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import MyForm
 from carrental.models import Client
 from django.http import HttpResponse
-from .models import Person
+from .models import Person, Company
 
 # Create your views here.
 
@@ -53,6 +53,50 @@ def registration_person(request):
     else:
         form = MyForm()
     return render(request, "client_registration.html", {'form': form})
+
+
+def registration_company(request):
+    if request.method == 'POST':
+        form = MyForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            repeated_password = form.cleaned_data['repeated_password']
+            phone = form.cleaned_data['phone']
+            country = form.cleaned_data['country']
+            nip = form.cleaned_data['nip']
+            name = form.cleaned_data['name']
+            sector = form.cleaned_data['sector']
+            print(country)
+            # Process the form data or save it to the database
+            is_ok = True
+            if password != repeated_password:
+                is_ok = False
+            if not phone.isnumeric():
+                is_ok = False
+            if not nip.isnumeric():
+                is_ok = False
+            if is_ok:
+                client = Client.objects.create(
+                    login=username, 
+                    email=email, 
+                    password=password,
+                    phone=phone,
+                    country=country
+                )
+                person = Company.objects.create(
+                    nip=nip,
+                    name=name,
+                    sector=sector,
+                    parent=client
+                )
+                person.save()
+                return redirect('/base/?text={}'.format("Successful registration"))
+            return render(request, "company_registration.html", {'form': form})
+    else:
+        form = MyForm()
+    return render(request, "company_registration.html", {'form': form})
 
 
 def check_log(request):
