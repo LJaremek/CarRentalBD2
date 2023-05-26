17995ef4aa10bc8a5183ce216508f47401b14b51
Paweł Dombrzalski <01169161@pw.edu.pl>
2023-05-26 17:52:48 +0200

    Updated procedure

--- carrentalbd2/procedures/utils.sql
+++ carrentalbd2/procedures/utils.sql
@@ -1,16 +1,17 @@
-CREATE OR REPLACE PROCEDURE remove_duplicate AS
+CREATE OR REPLACE PROCEDURE remove_duplicate()
+AS $$
 BEGIN
-  -- Update query
-  UPDATE carrental_person
-  SET client_ptr_id=parent_id
-  WHERE client_ptr_id!=parent_id;
+    -- Update query
+    UPDATE carrental_person
+    SET client_ptr_id = parent_id
+    WHERE client_ptr_id != parent_id;
   
-  -- Delete query
-  DELETE FROM carrental_client
-  WHERE login = '';
+    -- Delete query
+    DELETE FROM carrental_client
+    WHERE login = '';
   
-  -- Commit the changes
-  COMMIT;
+    -- Commit the changes
+    COMMIT;
 END;
-
-
+$$ 
+LANGUAGE plpgsql;
